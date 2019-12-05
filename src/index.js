a35437fc78bc24c9539efd5f5b29d7f56cc6877f
wendell.guimaraes <wendell.guimaraes@ntendencia.com.br>
2019-12-05 16:27:02 -0300

    comint 02

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-<div>
-    <h1>Calculadora</h1>
-    <Calculator />
-</div>
-,document.getElementById('root'));
-serviceWorker.unregister();
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+, document.getElementById('root'));
+registerServiceWorker();
